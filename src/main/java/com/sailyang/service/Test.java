32970e4dbd78f808cb4b1a0806966c8acb1ca9d7
sai-Yang <1186599081@qq.com>
2024-06-28 10:18:58 +0800

    feature：init

---
+++ src/main/java/com/sailyang/service/Test.java
@@ -0,0 +1,13 @@
+package com.sailyang.service;
+
+/**
+ * @author yangfan
+ * @version 1.0
+ * @description: TODO
+ * @date 2024/6/28 10:17
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/sailyang/service/UserService.java
@@ -0,0 +1,10 @@
+package com.sailyang.service;
+
+/**
+ * @author yangfan
+ * @version 1.0
+ * @description: TODO
+ * @date 2024/6/28 10:18
+ */
+public class UserService {
+}
