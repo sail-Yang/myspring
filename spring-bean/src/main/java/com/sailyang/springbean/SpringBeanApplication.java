3ef4341817e7167116abffc014b74aa47a1b84da
sai-Yang <1186599081@qq.com>
2024-08-09 11:04:19 +0800

    feature：spring-bean 从DefaultSingletonBeanRegistry获取单例Bean

--- spring-bean/src/main/java/com/sailyang/springbean/SpringBeanApplication.java
+++ spring-bean/src/main/java/com/sailyang/springbean/SpringBeanApplication.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.support.DefaultSingletonBeanRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.io.Resource;
 
 import java.lang.reflect.Field;
+import java.util.Locale;
 import java.util.Map;
 
 @SpringBootApplication
@@ -21,9 +23,11 @@ public class SpringBeanApplication {
             singletonObjects.setAccessible(true);
             ConfigurableListableBeanFactory beanFactory = context.getBeanFactory();
             Map<String,Object> stringObjectMap = (Map<String,Object>)singletonObjects.get(beanFactory);
-//            stringObjectMap.forEach((k,v)->{
-//                System.out.println(k+" : "+v);
-//            });
+            // 获取所有单例Bean
+            stringObjectMap.forEach((k,v)->{
+                System.out.println(k+" : "+v);
+            });
+            // 过滤单例Bean
             stringObjectMap.entrySet().stream().filter(e->e.getKey().startsWith("component"))
                     .forEach(e->{
                 System.out.println(e.getKey()+" : "+e.getValue());
